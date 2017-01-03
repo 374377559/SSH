d59a5909d34e7e63c7c922e9505e579b19cc6a05
Jack <374377559@qq.com>
2017-01-03 16:01:21 +0800

    完成多条件查询，时间段查询，以及投诉处理回复

--- WebContent/jsp/nsfw/complain/listUI.jsp
+++ WebContent/jsp/nsfw/complain/listUI.jsp
@@ -3,6 +3,7 @@
 <head>
     <%@include file="/common/header.jsp"%>
     <title>投诉受理管理</title>
+    <script type="text/javascript" src="${basePath}js/datepicker/WdatePicker.js"></script>
     <script type="text/javascript">
     	var list_url="${basePath}nsfw/complain_listUI.action";
 		//条件搜索
@@ -10,6 +11,10 @@
 			//重置页号
 			$("#pageNo").val(1);
 			document.forms[0].action = list_url;
+			document.forms[0].submit();
+		}
+		function doDeal(compId){
+			document.forms[0].action = "${basePath }nsfw/complain_dealUI.action?complain.compId=" + compId;
 			document.forms[0].submit();
 		}
     </script>
@@ -25,12 +30,15 @@
                        	投诉标题：<s:textfield name="complain.compTitle" cssClass="s_text"  cssStyle="width:160px;"/>
                     </li>
                     <li>
-                       	投诉时间：<s:textfield id="startTime" name="startTime" cssClass="s_text"  cssStyle="width:160px;"/>
+                       	投诉时间：<s:textfield id="startTime" name="startTime" cssClass="s_text"  cssStyle="width:160px;" 
+                       	readonly="true" onfocus="WdatePicker({'skin':'whyGreen','dateFmt':'yyyy-MM-dd HH:mm'});"/>
+                       	
                               - 
-                             <s:textfield id="endTime" name="endTime" cssClass="s_text"  cssStyle="width:160px;"/>
+                             <s:textfield id="endTime" name="endTime" cssClass="s_text"  cssStyle="width:160px;"
+                             readonly="true" onfocus="WdatePicker({'skin':'whyGreen','dateFmt':'yyyy-MM-dd HH:mm'});"/>
                     </li>
                     <li>
-                       	状态：<s:select name="complain.state" list="#complainStateMap"/>
+                       	状态：<s:select name="complain.state" list="#complainStateMap" headerKey="" headerValue="全部"/>
                     </li>
                     <li><input type="button" class="s_button" value="搜 索" onclick="doSearch()"/></li>
                     <li style="float:right;">
--- src/cn/web/controller/ComplainAction.java
+++ src/cn/web/controller/ComplainAction.java
@@ -1,14 +1,17 @@
 package cn.web.controller;
 
-import javax.annotation.Resource;
+import java.net.URLDecoder;
+import java.sql.Timestamp;
+import java.util.Date;
 
-import org.apache.struts2.dispatcher.ActionContextCleanUp;
+import javax.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.web.util.QueryHelper;
-
 import cn.web.entity.Complain;
-import cn.web.page.PageResult;
+import cn.web.entity.ComplainReply;
 import cn.web.service.ComplainService;
 
 /**
@@ -17,16 +20,110 @@ import cn.web.service.ComplainService;
  * 2016年12月30日上午9:00:51
  */
 public class ComplainAction extends BaseAction{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Resource
 	private ComplainService complainService;
 	private Complain complain;
+	private String startTime;
+	private String endTime;
+	private ComplainReply reply;
 	
-	public String listUI(){
+	public String listUI() throws Exception{
 		//加载状体
 		ActionContext.getContext().put("complainStateMap", Complain.COMPLAIN_STATE_MAP);
-		 QueryHelper query = new QueryHelper(Complain.class, "c");
-		 pageResult = complainService.getPageResult(query, getPageNo(), getPageSize());
+		 QueryHelper queryHelper = new QueryHelper(Complain.class, "c");
+		 if(StringUtils.isNotBlank(startTime)){//查询开始时间之后的投诉数据
+			 startTime = URLDecoder.decode(startTime,"utf-8");
+			 queryHelper.addCondition("c.compTime >= ?", DateUtils.parseDate(startTime+":00", "yyyy-MM-dd HH:mm:ss"));
+		 }
+		 if(StringUtils.isNotBlank(endTime)){//查询结束时间之前的投诉数据
+				endTime = URLDecoder.decode(endTime, "utf-8");
+				queryHelper.addCondition("c.compTime <= ?", DateUtils.parseDate(endTime+":59", "yyyy-MM-dd HH:mm:ss"));
+		  }
+		 
+		 if(complain != null){
+			 if(StringUtils.isNotBlank(complain.getState())){
+				 queryHelper.addCondition("c.state=?",complain.getState());
+			 }
+			 if(StringUtils.isNotBlank(complain.getCompTitle())){
+				 complain.setCompTitle(URLDecoder.decode(complain.getCompTitle(),"utf-8"));
+				 queryHelper.addCondition("c.compTitle like ?","%" + complain.getCompTitle() + "%");
+			 }
+			 
+		 }
+	 	//安装状态升序排序
+		queryHelper.addOrderByProperty("c.state", QueryHelper.ORDER_BY_ASC);
+		//按照投诉时间升序排序
+		queryHelper.addOrderByProperty("c.compTime", QueryHelper.ORDER_BY_ASC);
+		 
+		pageResult = complainService.getPageResult(queryHelper, getPageNo(), getPageSize());
 		return "listUI";
 	}
+	
+	//跳转到受理页面
+	public String dealUI(){
+		//加载状态集合
+		ActionContext.getContext().getContextMap().put("complainStateMap", Complain.COMPLAIN_STATE_MAP);
+		if(complain != null){
+			complain = complainService.findObjectById(complain.getCompId());
+		}
+		return "dealUI";
+	}
+	
+	
+	public String deal(){
+		if(complain != null){
+			Complain tem = complainService.findObjectById(complain.getCompId());
+			//1、更新投诉的状态为 已受理
+			if(!Complain.COMPLAIN_STATE_DONE.equals(tem.getState())){//更新状态为 已受理
+				tem.setState(Complain.COMPLAIN_STATE_DONE);
+			}
+			//2、保存回复信息
+			if(reply != null){
+				reply.setComplain(tem);
+				reply.setReplyTime(new Timestamp(new Date().getTime()));
+				tem.getComplainReplies().add(reply);
+			}
+			complainService.update(tem);
+		}
+		return "list";
+	}
+
+	public Complain getComplain() {
+		return complain;
+	}
+
+	public void setComplain(Complain complain) {
+		this.complain = complain;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+	
+	public ComplainReply getReply() {
+		return reply;
+	}
+
+	public void setReply(ComplainReply reply) {
+		this.reply = reply;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	
+	
 	
 }
